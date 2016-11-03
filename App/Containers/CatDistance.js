286eed1834528c1ddc0ab0c7b741110727e32d98
Andrew Welton <awelton@mail.uoguelph.ca>
2016-11-03 09:07:49 -0400

    buttons are actually things now

--- App/Containers/CatDistance.js
+++ App/Containers/CatDistance.js
@@ -1,10 +1,43 @@
 import React from 'react'
 import Moment from 'moment'
-import { AsyncStorage, Text, TouchableOpacity, View, ScrollView } from 'react-native'
+import { AsyncStorage, Text, TouchableOpacity, TouchableHighlight, View, Image, ScrollView, StyleSheet } from 'react-native'
 import GraphComponent from '../Components/GraphComponent'
+import Icon from "react-native-vector-icons/FontAwesome"
+import { Images } from '../Themes'
 import { default as StorageKeys } from '../Config/StorageKeys'
 
 import styles from './Styles/CatDistanceStyle'
+
+const tempStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 8,
+  },
+  touchable: {
+    overflow: 'hidden',
+  },
+  iconLeft: {
+    color: 'white',
+    marginRight: 10,
+  },
+  iconRight: {
+    color: 'white',
+    marginLeft: 10,
+  },
+  text: {
+    fontWeight: '600',
+    backgroundColor: 'transparent',
+  },
+  blockStyle: {
+    backgroundColor: 'blue',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+  }
+});
 
 export default class CatDistance extends React.Component {
   constructor (props) {
@@ -81,20 +114,21 @@ export default class CatDistance extends React.Component {
     return (
       <View style={styles.mainContainer}>
         <ScrollView style={styles.container}>
-          <Text style={styles.sectionText}>
-            Cat distance!
-          </Text>
-          <Text style={styles.sectionText} onPress={this.onPressLeft}>
-            &#60;
-          </Text>
+          <TouchableHighlight style={[tempStyles.touchable, tempStyles.blockStyle]} onPress={this.onPressLeft}>
+            <View style={[tempStyles.container, tempStyles.blockStyle]}>
+              <Icon name="chevron-left" style={tempStyles.iconLeft} />
+              <Text style={tempStyles.buttonText}>Previous Week</Text>
+            </View>
+          </TouchableHighlight>
           <Text style={styles.sectionText}>
             {this.state.selectedDate.format('dddd DD, MMMM YYYY') + ' - ' + Moment(this.state.selectedDate).add(7, 'day').format('dddd DD, MMMM YYYY')}
           </Text>
-          <TouchableOpacity disabled={this.canMoveForward()} onPress={this.onPressRight}>
-            <Text style={styles.sectionText}>
-                &#62;
-            </Text>
-          </TouchableOpacity>
+          <TouchableHighlight style={[tempStyles.touchable, tempStyles.blockStyle]} onPress={this.onPressRight}>
+            <View style={[tempStyles.container, tempStyles.blockStyle]}>
+              <Text style={tempStyles.buttonText}>Next Week</Text>
+              <Icon name="chevron-right" style={tempStyles.iconRight} />
+            </View>
+          </TouchableHighlight>
           <GraphComponent weekData={this.state.currentWeekData} />
         </ScrollView>
       </View>
