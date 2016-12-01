9f3240fdce1be4c124c4f56e28c2d098a0a7ed09
Michelle MacKenzie <michelle@Michelles-MacBook-Pro.local>
2016-11-30 20:54:58 -0800

    fix dropdowns

--- App/Components/Dropdown.js
+++ App/Components/Dropdown.js
@@ -20,7 +20,7 @@ export default class Dropdown extends React.Component {
       items.push(<Picker.Item label={I18n.t('selectOne')} value={-1} key={'first'} />)
 
       for (var i = 0; i < options.length; i++) {
-        items.push(<Picker.Item label={options[i].name} value={options[i].value} key={i} />)
+        items.push(<Picker.Item label={options[i].name} value={options[i].id} key={i} />)
       }
 
       this.setState({
