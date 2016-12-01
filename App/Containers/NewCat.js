e689f4fd1370a20009b2bcdca1bcec9c8cd6a8c9
Michelle MacKenzie <michelle@Michelles-MacBook-Pro.local>
2016-11-30 19:35:30 -0800

    fixed date picker

--- App/Containers/NewCat.js
+++ App/Containers/NewCat.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Alert, DatePickerAndroid, DatePickerIOS, Platform, ScrollView, Switch, Text, TextInput, TouchableHighlight, View } from 'react-native'
+import { Alert, ScrollView, Switch, Text, TextInput, View } from 'react-native'
 import { Actions as NavigationActions } from 'react-native-router-flux'
 import I18n from 'react-native-i18n'
+import DatePicker from 'react-native-datepicker'
 import { Colors } from '../Themes'
 import RoundedButton from '../Components/RoundedButton'
 import Dropdown from '../Components/Dropdown'
@@ -18,7 +19,10 @@ export default class NewCat extends React.Component {
       type: 1,
       breed: -1,
       gender: -1,
-      dob: new Date(),
+      dob: 'Y-m-d'
+      .replace('Y', new Date().getFullYear())
+      .replace('m', new Date().getMonth() + 1)
+      .replace('d', new Date().getDate()),
       weight: '',
       height: '',
       length: '',
@@ -26,8 +30,7 @@ export default class NewCat extends React.Component {
       outdoor: false,
       fixed: false,
       breeds: {},
-      genders: {},
-      timeZoneOffsetInMinutes: (-1) * (new Date().getTimezoneOffset())
+      genders: {}
     }
   }
 
@@ -62,12 +65,7 @@ export default class NewCat extends React.Component {
   }
 
   createCat (name, type, breed, gender, dob, weight, height, length, declawed, outdoor, fixed) {
-    var dobFormatted = 'Y-m-d'
-      .replace('Y', dob.getFullYear())
-      .replace('m', dob.getMonth() + 1)
-      .replace('d', dob.getDate())
-
-    WellCatManager.newAnimal(name, type, breed, gender, dobFormatted, weight, height, length, declawed, outdoor, fixed)
+    WellCatManager.newAnimal(name, type, breed, gender, dob, weight, height, length, declawed, outdoor, fixed)
       .then((result) => {
         if (result.code === 1) {
           NavigationActions.addCat()
@@ -81,127 +79,7 @@ export default class NewCat extends React.Component {
       })
   }
 
-  async showPicker () {
-    try {
-      await DatePickerAndroid.open({date: /* this.state.dob */ new Date()}) // commented is undefined
-        .then(({action, year, month, day}) => {
-          if (action !== DatePickerAndroid.dismissedAction) {
-            this.setState({dob: new Date(year, month, day)})  // undefined, date is fine
-          }
-        })
-    } catch (error) {
-      // Alert.alert(`${I18n.t('tryLater')}`)
-      console.log(error)
-    }
-  }
-
   render () {
-    if (Platform.OS === 'ios') {
-      return (
-        <View style={styles.mainContainer}>
-          <ScrollView style={styles.container}>
-            <Text style={styles.sectionText} >
-              {I18n.t('welcomeNewCat')}
-            </Text>
-
-            <View style={styles.section} >
-              <Text style={styles.sectionText} >
-                {I18n.t('name')}
-              </Text>
-              <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10}}
-                placeholder={I18n.t('name')}
-                placeholderTextColor={Colors.placeholderText}
-                onChangeText={(name) => this.setState({name})}
-                autoCapitalize={'none'}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('breed')}
-              </Text>
-              <Dropdown
-                options={this.state.breeds}
-                selectedValue={this.state.breed}
-                onValueChange={(breed) => this.setState({breed})}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('gender')}
-              </Text>
-              <Dropdown
-                options={this.state.genders}
-                selectedValue={this.state.gender}
-                onValueChange={(gender) => this.setState({gender})}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('dob')}
-              </Text>
-              <DatePickerIOS
-                date={this.state.dob}
-                mode='date'
-                timeZoneOffsetInMinutes={this.state.timeZoneOffsetInMinutes}
-                onDateChange={(dob) => this.setState({dob})}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('weight')}
-              </Text>
-              <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10}}
-                placeholder={I18n.t('weight')}
-                placeholderTextColor={Colors.placeholderText}
-                onChangeText={(weight) => this.setState({weight})}
-                autoCapitalize={'none'}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('height')}
-              </Text>
-              <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10}}
-                placeholder={I18n.t('height')}
-                placeholderTextColor={Colors.placeholderText}
-                onChangeText={(height) => this.setState({height})}
-                autoCapitalize={'none'}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('length')}
-              </Text>
-              <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1, paddingLeft: 10}}
-                placeholder={I18n.t('length')}
-                placeholderTextColor={Colors.placeholderText}
-                onChangeText={(length) => this.setState({length})}
-                autoCapitalize={'none'}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('declawed')}
-              </Text>
-              <Switch
-                value={this.state.declawed}
-                onValueChange={(declawed) => this.setState({declawed})}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('outdoor')}
-              </Text>
-              <Switch
-                value={this.state.outdoor}
-                onValueChange={(outdoor) => this.setState({outdoor})}
-              />
-              <Text style={styles.sectionText} >
-                {I18n.t('fixed')}
-              </Text>
-              <Switch
-                value={this.state.fixed}
-                onValueChange={(fixed) => this.setState({fixed})}
-              />
-            </View>
-
-            <View style={styles.paddedContainer}>
-              <RoundedButton onPress={() => this.createCat(this.state.name, this.state.type, this.state.breed, this.state.gender, this.state.dob, this.state.weight, this.state.height, this.state.length, this.state.declawed, this.state.outdoor, this.state.fixed)}>
-                {I18n.t('addNewCat')}
-              </RoundedButton>
-            </View>
-          </ScrollView>
-        </View>
-      )
-    }
     return (
       <View style={styles.mainContainer}>
         <ScrollView style={styles.container}>
@@ -239,11 +117,14 @@ export default class NewCat extends React.Component {
             <Text style={styles.sectionText} >
               {I18n.t('dob')}
             </Text>
-            <TouchableHighlight onPress={this.showPicker}>
-              <Text>
-                {`${this.state.dob}`}
-              </Text>
-            </TouchableHighlight>
+            <DatePicker
+              date={this.state.dob}
+              mode='date'
+              format='YYYY-MM-DD'
+              confirmBtnText={I18n.t('confirm')}
+              cancelBtnText={I18n.t('cancel')}
+              onDateChange={(dob) => this.setState({dob})}
+            />
             <Text style={styles.sectionText} >
               {I18n.t('weight')}
             </Text>
