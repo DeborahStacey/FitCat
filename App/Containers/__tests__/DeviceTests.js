9b13a32fde4fdd52bf0bc06c7fa32670e139dcfc
Ian Pavlinic <ipavlini@mail.uoguelph.ca>
2016-12-09 02:42:31 -0500

    Fix Device sync time showing the current time instead of the actual sync time

--- App/Containers/__tests__/DeviceTests.js
+++ App/Containers/__tests__/DeviceTests.js
@@ -7,7 +7,7 @@ import renderer from 'react-test-renderer'
 
 jest.mock('../../Services/MomentWrapper', () => {
   return {
-    getNewMoment: () => { return new Moment('20161212') }
+    getMomentFromDate: () => { return new Moment('20161212') }
   }
 })
 it('renders the device page correctly', () => {
