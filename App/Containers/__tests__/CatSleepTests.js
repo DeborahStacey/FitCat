f5e64758cf3d99aab39c7e430dcb9ceb749be37c
Ian Pavlinic <ipavlini@mail.uoguelph.ca>
2016-12-09 02:26:36 -0500

    Mock dates on WeeklyActivityBase screen tests

--- App/Containers/__tests__/CatSleepTests.js
+++ App/Containers/__tests__/CatSleepTests.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import CatSleep from '../CatSleep'
+import Moment from 'moment'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
+jest.mock('../../Services/MomentWrapper', () => {
+  return {
+    getMomentFromDate: () => { return new Moment('20161212') }
+  }
+})
 it('renders the cat sleep page correctly', () => {
   const tree = renderer.create(
     <CatSleep />
